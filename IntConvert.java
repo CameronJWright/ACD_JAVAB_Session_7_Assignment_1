6ea794e5f6e0a7996c45d9a54c2d7f0598122c81
CameronJWright <53836742+CameronJWright@users.noreply.github.com>
2019-08-14 16:06:45 -0400

    Add files via upload

---
+++ IntConvert.java
@@ -0,0 +1,18 @@
+package session7;
+
+import java.util.Scanner;
+
+public class IntConvert {
+
+	public static void main(String[] args) {
+		int n;
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter int.");
+		n=scan.nextInt();
+		String input = String.valueOf(n);
+		System.out.println(input);
+		
+	}
+
+}
